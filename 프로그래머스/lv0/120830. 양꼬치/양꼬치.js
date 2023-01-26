0e63be096a5f7e42b24ce169862782e298fd066a
jakejun98 <111010564+jakejun98@users.noreply.github.com>
2023-01-26 11:12:52 +0900

    [level 0] Title: 양꼬치, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,15 @@
+function solution(n, k) {
+    var answer = 0;
+    
+    for(i=1;i<=n;i++){
+        if(i%10===0){
+            k--;
+        }
+            
+    
+    }
+    
+    answer = 12000*n + 2000*k;
+    
+    return answer 
+}
