d365d21b76d01839bff6f8a39d97bcae9b9c413c
jake <111010564+jakejun98@users.noreply.github.com>
2023-02-12 22:07:24 +0900

    [level 0] Title: 외계어 사전, Time: 8.39 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,6 +1,5 @@
 function solution(spell, dic) {
     let result = 2;
-    const newArray = [];
     const filtered = dic.filter((n)=> n.length === spell.length)
     const spelled = spell.sort(function(a, b) {
   return a.localeCompare(b);
