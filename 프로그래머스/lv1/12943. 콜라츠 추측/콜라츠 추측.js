842d704a28e8f6fffa7e5d7f76fdb95c8857f09a
양성준 <111010564+jakejun98@users.noreply.github.com>
2023-05-12 13:40:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,22 @@
+function solution(num) {
+    let count=0;
+    while(num!=1){
+        if(num%2==0){
+           num=  num/2 ;
+            
+        }else{
+          num =  (num*3)+1;
+           
+        }
+        count = count + 1;
+    }
+    if(count>=500){
+       return -1
+    }
+     return count   
+}
+/*if(num%2==0)
+    num/2 
+    if(num%2=!0)
+    (num*3)+1
+*/    
