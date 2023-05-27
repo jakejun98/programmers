f65331c39af3473cc7c02a6b92111e76096561e9
양성준 <111010564+jakejun98@users.noreply.github.com>
2023-05-28 01:15:27 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,18 +1,14 @@
 function solution(s) {
   let al= ['zero','one','two','three','four','five','six','seven',    'eight','nine']
   let num = [0,1,2,3,4,5,6,7,8,9]
-   
+  let index = 0;
+  let index1=0;  
     
-    
-  for (let i = 0; i < al.length; i++) {
-  let index = s.indexOf(al[i]);
-  if (index !== -1) {
-    let replacement = num[i];
-    s = s.replace(new RegExp(al[i], 'g'), replacement);
+  for(let i=0;i<al.length;i++){
+     if(s.includes(al[i])){
+          s = s.slice(0,i) + s.slice(i)
+   }     
   }
-      
-}
     
-    return Number(s)
-}    
- //"one4seveneight"
+  return s
+}  //"one4seveneight"
