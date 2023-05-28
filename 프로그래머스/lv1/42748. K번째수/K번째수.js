6f6ce4392eee09740e05ebcef18e01e9f17fa07d
양성준 <111010564+jakejun98@users.noreply.github.com>
2023-05-29 00:14:29 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+    let newArray = []
+    let result = []
+    for(let i=0;i<commands.length;i++){
+       newArray.push(array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b))
+        result.push(newArray[i][commands[i][2]-1])   
+        
+    }
+    return result
+}
