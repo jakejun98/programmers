3ae7796f436bceb82c647c3445414298aadbf509
양성준 <111010564+jakejun98@users.noreply.github.com>
2023-11-17 20:51:27 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 3.29 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,9 @@
+function solution(myString, pat) {
+    const lowerCaseStr = myString.toLowerCase();
+    const lowerPat = pat.toLowerCase();
+    console.log(lowerCaseStr.split(" "))
+    if(lowerCaseStr.includes(lowerPat)){
+        
+    return 1
+    }else return 0
+}
