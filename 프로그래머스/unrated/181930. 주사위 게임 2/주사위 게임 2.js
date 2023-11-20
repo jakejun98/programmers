6a9754ba1caad4e4d07f798110f8a0d8c3b0a945
양성준 <111010564+jakejun98@users.noreply.github.com>
2023-11-21 01:01:26 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,7 @@
+function solution(a, b, c) {
+    if(a!=b && b!=c && a!=c){
+        return a+b+c
+    }else if(a==b && b==c && a==c){
+        return (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c)
+    }else return (a+b+c)*(a*a+b*b+c*c)
+}
