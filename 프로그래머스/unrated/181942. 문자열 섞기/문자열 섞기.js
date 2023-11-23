0d6a3e33ea055dfa39e739cfbfa4dc960c09109a
양성준 <111010564+jakejun98@users.noreply.github.com>
2023-11-23 19:49:21 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -0,0 +1,9 @@
+function solution(str1, str2) {
+    const answer = [];
+    for(let i=0;i<str1.length;i++){
+        answer.push(str1[i])
+        answer.push(str2[i])
+    }
+    
+    return answer.join("");
+}
