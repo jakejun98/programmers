da45f7c0130416575a19ce036e4fa98442c13703
양성준 <111010564+jakejun98@users.noreply.github.com>
2024-05-16 21:37:10 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -34,6 +34,7 @@
 }  */
 function solution(n) {
     let oneNum = n.toString(2).split("1").length;
+   
     while (true) {
       n++;
       if (n.toString(2).split("1").length === oneNum) return n;
