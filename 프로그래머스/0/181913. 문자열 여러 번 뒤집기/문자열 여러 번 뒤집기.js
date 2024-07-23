dd12939074ab6030365fbe0481148dd01d9da977
양성준 <111010564+jakejun98@users.noreply.github.com>
2024-07-23 13:30:38 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 17.15 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,11 +1,4 @@
 function solution(my_string, queries) {
-  for (let i = 0; i < queries.length; i++) {
-    const [start, end] = queries[i];
-    const reversed = [...my_string]
-      .slice(start, end + 1)
-      .reverse()
-      .join("");
-    my_string = my_string.slice(0, start) + reversed + my_string.slice(end + 1);
-  }
-  return my_string;
+    var answer = '';
+    return answer;
 }
