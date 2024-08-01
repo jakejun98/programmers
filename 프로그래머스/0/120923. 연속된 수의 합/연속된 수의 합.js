6e64a8539cc9b6b8bfb8e28cdd53a3deee90adf8
양성준 <111010564+jakejun98@users.noreply.github.com>
2024-08-01 17:25:56 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.89 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -9,3 +9,26 @@ function solution(num, total) {
         }
     }
 }
+
+
+/* 수학적으로 이렇게 구할수도있음 
+
+sum=i+(i+1)+(i+2)+⋯+(i+num−1)
+sum= num/2 * (2i +num -1)
+이걸 i에 대하여 정리 한것이 i의 시작값 
+
+
+function solution(num, total) {
+    // 수학적으로 구한 시작 값
+    let start = Math.floor((total - (num * (num - 1)) / 2) / num);
+    
+    // 연속된 숫자 배열 생성
+    const sumArray = [];
+    for (let i = 0; i < num; i++) {
+        sumArray.push(start + i);
+    }
+    
+    return sumArray;
+}
+
+*/
