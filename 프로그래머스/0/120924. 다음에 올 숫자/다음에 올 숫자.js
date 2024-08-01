b4fc825c8b7f50e5c12d54733e4503e1a162a85f
양성준 <111010564+jakejun98@users.noreply.github.com>
2024-08-01 15:28:28 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,7 +1,6 @@
 function solution(common) {
-    let count = 0;
     
-    // 등차
+    
     if(common[1]-common[0] == common[2]-common[1]){
         return common[common.length-1] + common[1]-common[0]
     }else{
