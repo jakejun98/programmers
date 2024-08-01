a42877e2b82ddf1d44922524e99e6503b7a2fc46
양성준 <111010564+jakejun98@users.noreply.github.com>
2024-08-01 15:17:40 +0900

    [level 1] Title: 카드 뭉치, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,4 +1,47 @@
 function solution(cards1, cards2, goal) {
+            let i=0;
+            let j=0;
+            let k=0;
+            let g=0;
+            let h=0;
+    
+        while(i<goal.length){
+            
+            if(cards1[j]==goal[i]){
+                i++;
+                j++;
+                continue;
+            }else {
+                if(cards2[k]==goal[i]){
+                    i++;
+                    k++;
+                    continue;
+                }else {
+                    return "No"
+                }
+            }
+            if(cards2[g]==goal[i]){
+                i++;
+                g++;
+                continue;
+            }else {
+                if(cards1[h]==goal[i]){
+                    i++;
+                    h++
+                    continue;
+                    
+                }else {
+                    return "No"
+                }
+            }
+            
+    
+        }
+        
+      return "Yes"
+} 
+
+/* function solution(cards1, cards2, goal) {
     let i = 0;
     let j = 0;
     let k = 0;
@@ -16,3 +59,4 @@ function solution(cards1, cards2, goal) {
 
     return "Yes";
 }
+*/
