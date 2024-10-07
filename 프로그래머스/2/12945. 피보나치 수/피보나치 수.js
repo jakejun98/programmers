91353eac74493614f1cee1c8d4471d1a10c4a91a
양성준 <111010564+jakejun98@users.noreply.github.com>
2024-10-08 08:55:05 +0900

    [level 2] Title: 피보나치 수, Time: 4.72 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,5 +1,4 @@
 function solution(n) {
-    let fibonacci = [0, 1];
-    for(let i = 2; i <= n; i++) fibonacci[i] = (fibonacci[i-1] + fibonacci[i-2]) % 1234567;
-    return fibonacci[n];
+    var answer = 0;
+    return answer;
 }
