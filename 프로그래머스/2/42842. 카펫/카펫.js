0b8e3cb8deed6ea9627f51149b0f28cf721d9487
양성준 <111010564+jakejun98@users.noreply.github.com>
2024-10-08 11:34:09 +0900

    [level 2] Title: 카펫, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -9,3 +9,4 @@ function solution(brown, yellow) {
  
     return [x,y]; 
 }
+// var를 사용했기 때문에 for 루프 안에서 선언된 x는 루프가 끝난 후에도 유지되어 return 문에서 사용할 수 있습니다. 그러나 let을 사용하여 변수를 명확히 블록 스코프로 선언하는 것이 더 나은 코드 스타일입니다.
