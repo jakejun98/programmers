3fd2161ab379d97ea2f50cbff02822e98cdd176f
양성준 <111010564+jakejun98@users.noreply.github.com>
2024-11-15 10:50:17 +0900

    [level 2] Title: 올바른 괄호, Time: 32.52 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,17 @@
+function solution(s){
+   
+    let count = 0;
+    for(let char of s){
+        if(char==='('){
+            count++
+        }else count--
+        
+        if(count<0){
+            return false
+        }
+    }
+    
+    if(count===0){
+        return true
+    }else return false
+}
